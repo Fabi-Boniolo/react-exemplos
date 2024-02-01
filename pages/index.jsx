09b46756ef279eb5751fb4df55600d8921020bb4
Fabi-Boniolo <fabianaboniolo@gmail.com>
2024-02-01 16:30:38 -0300

    Criado useState

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,18 @@
+import { useState} from 'react'
+
 import Head from "next/head"
 
 import Title from "../src/componentes/title/Title"
 import Subtitle from "../src/componentes/subtitle/Subtitle"
+import Button from "../src/componentes/button/Button"
+
 
 function HomePage() {
+    const [click, setClick] = useState(0)
+
+    const handleClick = () => {
+        setClick(click + 1)
+    }
     return (
         <>
             <Head>
@@ -13,7 +22,13 @@ function HomePage() {
             <Title>Um titulo</Title>
             <Subtitle text="Um subtitulo" />
             <Title>Outro titulo</Title>
-            <Subtitle text="Outro subtitulo"/>
+            <Subtitle text="Outro subtitulo" />
+                <Button
+                    onClick={handleClick}
+                >
+                    Botão teste
+                </Button>
+                <p>Cliques: { click }</p>
             </div>
         </>
             )   
